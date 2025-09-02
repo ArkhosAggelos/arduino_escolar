6a42b41ac0f3ad9102b827c05451d17e11a70f40
Claudio Roberto da Silva <108074829+ArkhosAggelos@users.noreply.github.com>
2025-09-02 14:00:20 -0300

    Update exemplo1.ino

--- LDR/exemplo1.ino
+++ LDR/exemplo1.ino
@@ -3,7 +3,13 @@
     Com um circuito simples contendo apenas um LDR e um resistor, conforme
     o esquema abaixo:
 
+    (5V) --- [LDR] ---+--- [Resistor] --- (GND)
+                  |
+               (A0)
     
+    O resistor pode ser de 10k para começar, pode-se mudar esse valor como 
+    for necessário para ajustar a faixa de valores..
+
     By Professor Claudio Roberto da Silva
 */
 
