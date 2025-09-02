ed95426682a277b1a7465fe9185374945d4bd9c5
Claudio Roberto da Silva <108074829+ArkhosAggelos@users.noreply.github.com>
2025-09-02 13:54:43 -0300

    Update exemplo1.ino

--- LDR/exemplo1.ino
+++ LDR/exemplo1.ino
@@ -1,5 +1,23 @@
 /* 
-    Código didático de exemplo para uso em aulas de programação e robótica 
-    
+    Código didático de exemplo para uso em aulas de programação e robótica.
+    Com um circuito simples contendo apenas um LDR e um resistor, conforme
+    o esquema abaixo:
 
+    
+    By Professor Claudio Roberto da Silva
 */
+
+#define pinoLDR A0                    // Defina o pino onde o LDR está conectado
+
+void setup() {
+  Serial.begin(9600);                 // Inicializa a comunicação serial
+}
+
+void loop() {
+  int valorLDR = analogRead(pinoLDR); // Lê o valor da entrada analógica
+
+  Serial.print("Valor do LDR: ");
+  Serial.println(valorLDR);           // Exibe o valor no Monitor Serial
+
+  delay(1000);                        // Aguarda um segundo entre as leituras
+}
