5e9aa2eb5f3790eb772e783026297054c1dc1782
Claudio Roberto da Silva <108074829+ArkhosAggelos@users.noreply.github.com>
2025-09-02 14:09:01 -0300

    Create exemplo2.ino

---
+++ LDR/exemplo2.ino
@@ -0,0 +1,14 @@
+/* 
+    Código didático de exemplo para uso em aulas de programação e robótica.
+    Com um circuito simples contendo um LDR, um resistor e um led, conforme
+    o esquema abaixo:
+
+    (5V) --- [LDR] ---+--- [Resistor] --- (GND)
+                  |
+               (A0)
+    
+    O resistor pode ser de 10k para começar, pode-se mudar esse valor como 
+    for necessário para ajustar a faixa de valores..
+
+    ©2025 By Professor Claudio Roberto da Silva
+*/
